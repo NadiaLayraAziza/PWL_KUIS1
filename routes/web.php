065e02aa83844cbf5675289345b66e6ebec3ee62
Tita Wijayanti <wijayantitita094@gmail.com>
2021-03-10 21:46:45 +0700

    Membuat route untuk view index dan contact pada web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +21,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/contact', [ContactController::class, 'contact']);
