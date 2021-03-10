3a582c4e0d6d7326b1939e480fc830fa478e4143
NadiaLayraAziza <nadialayra15@gmail.com>
2021-03-10 22:38:54 +0700

    Menambahkan use controller company dan service pada web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
