361be9ed498dfac23accb28577f845ccad5c6b15
NadiaLayraAziza <nadialayra15@gmail.com>
2021-03-10 22:13:06 +0700

    Membuat master view navbar dan mengganti link navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,37 +34,7 @@ https://templatemo.com/tm-542-new-vision
                     <div class="tm-site-header-right tm-menu-container-outer">
 
                         <!--Navbar-->
-                        <nav class="navbar navbar-expand-lg">
-
-                          <!-- Collapse button -->
-                          <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
-                            aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
-                                class="fas fa-bars fa-1x"></i></span></button>
-
-                          <!-- Collapsible content -->
-                          <div class="collapse navbar-collapse tm-nav" id="navbarSupportedContent1">
-
-                            <!-- Links -->
-                            <ul class="navbar-nav mr-auto">
-                              <li class="nav-item active">
-                                <a class="nav-link tm-nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link tm-nav-link" href="company.html">Our Company</a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link tm-nav-link" href="services.html">Services</a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link tm-nav-link" href="contact.html">Contact</a>
-                              </li>
-                            </ul>
-                            <!-- Links -->
-
-                          </div>
-                          <!-- Collapsible content -->
-
-                        </nav>
+                        @include('layouts.navbar')
                         <!--/.Navbar-->
                     </div>
                 </div>
