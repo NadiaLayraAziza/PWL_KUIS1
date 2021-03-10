dc81116ad25622c063092ebcc6e9015351207f65
Tita Wijayanti <wijayantitita094@gmail.com>
2021-03-11 00:29:48 +0700

    Memperbaiki halaman service not found

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -10,14 +10,14 @@
 
       <!-- Links -->
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
+        <li class="nav-item">
           <a class="nav-link tm-nav-link" href="{{url('/')}}">Home</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link tm-nav-link" href="{{url('/company')}}">Our Company</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link tm-nav-link" href="{{url('/services')}}">Services</a>
+          <a class="nav-link tm-nav-link" href="{{url('/service')}}">Services</a>
         </li>
         <li class="nav-item">
           <a class="nav-link tm-nav-link" href="{{url('/contact')}}">Contact</a>
