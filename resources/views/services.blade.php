476bbfc66692786b569122942ea9062e881dff60
NadiaLayraAziza <nadialayra15@gmail.com>
2021-03-11 00:17:44 +0700

    Membuat view halaman service

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,73 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
-    <title>New Vision - Page 3</title>
-    
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800" rel="stylesheet" /> <!-- https://fonts.google.com/specimen/Open+Sans?selection.family=Open+Sans -->
-    <link href="css/all.min.css" rel="stylesheet" /> <!-- https://fontawesome.com/ -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" /> <!-- https://getbootstrap.com -->
-	<link href="css/templatemo-new-vision.css" rel="stylesheet" />
-<!--
-
-New Vision Template
-
-https://templatemo.com/tm-542-new-vision
-
--->
-</head>
-<body>
-    <!-- Page Header -->
-    <div class="container-fluid">
-        <div class="tm-site-header">
-            <div class="row">
-                <div class="col-12 tm-site-header-col">
-                    <div class="tm-site-header-left">
-                        <i class="far fa-2x fa-eye tm-site-icon"></i>
-                        <h1 class="tm-site-name">New Vision</h1>
-                    </div>
-                    <div class="tm-site-header-right tm-menu-container-outer">
-                        
-                        <!--Navbar-->
-                        <nav class="navbar navbar-expand-lg">
-                        
-                          <!-- Collapse button -->
-                          <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
-                            aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
-                                class="fas fa-bars fa-1x"></i></span></button>
-                        
-                          <!-- Collapsible content -->
-                          <div class="collapse navbar-collapse tm-nav" id="navbarSupportedContent1">
-                        
-                            <!-- Links -->
-                            <ul class="navbar-nav mr-auto">
-                              <li class="nav-item">
-                                <a class="nav-link tm-nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link tm-nav-link" href="company.html">Our Company</a>
-                              </li>
-                              <li class="nav-item active">
-                                <a class="nav-link tm-nav-link" href="#">Services</a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link tm-nav-link" href="contact.html">Contact</a>
-                              </li>
-                            </ul>
-                            <!-- Links -->
-                        
-                          </div>
-                          <!-- Collapsible content -->
-                        
-                        </nav>
-                        <!--/.Navbar-->
-                    </div>
-                </div>
-            </div>
-        </div>
+@extends('layouts.app')
+@section('content')
         
         <div class="row">
             <div class="col-12">
@@ -75,9 +7,6 @@ https://templatemo.com/tm-542-new-vision
             </div>
         </div>
         
-        <!-- Main -->
-        <main>
-            <!-- Welcome section -->
             <section class="tm-welcome">
                 <div class="row">
                     <div class="col-12">
@@ -184,17 +113,4 @@ https://templatemo.com/tm-542-new-vision
                     
                 </div>
             </section>
-            
-            <footer>
-                Copyright &copy; 2020 New Vision 
-                
-                - Design: <a href="https://templatemo.com" rel="sponsored" target="_parent" title="css templates">TemplateMo</a>
-            </footer>
-            
-        </main>
-    </div>
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/templatemo-script.js"></script>
-</body>
-</html>
+    @endsection
