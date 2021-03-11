063b16f06f1e6d43ee76faf5079c1f23e1493cc8
NadiaLayraAziza <nadialayra15@gmail.com>
2021-03-11 21:11:59 +0700

    Meng-insert data pegawai dalam seeder dan memanggil pegawaiSeeder dalam DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(BarangSeeder::class);
         $this->call(PelangganSeeder::class);
+        $this->call(PegawaiSeeder::class);
     }
 }
--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -13,6 +13,77 @@ class PegawaiSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\Pegawai::insert([
+            [
+                'name' => 'JOHN STONE',
+                'fb' => 'https://fb.com',
+                'twitter' => 'https://twitter.com',
+                'ig' => 'https://instagram.com',
+                'featured_image' => '/img/team-img-01.jpg'
+            ],
+            [
+                'name' => 'JANE MELDRUM',
+                'fb' => 'https://fb.com',
+                'twitter' => 'https://twitter.com',
+                'ig' => 'https://instagram.com',
+                'featured_image' => '/img/team-img-02.jpg'
+            ],
+            [
+                'name' => 'TREVOR VIRTUE',
+                'fb' => 'https://fb.com',
+                'twitter' => 'https://twitter.com',
+                'ig' => 'https://instagram.com',
+                'featured_image' => '/img/team-img-03.jpg'
+            ],
+            [
+                'name' => 'EMILY MOORE',
+                'fb' => 'https://fb.com',
+                'twitter' => 'https://twitter.com',
+                'ig' => 'https://instagram.com',
+                'featured_image' => '/img/team-img-04.jpg'
+            ],
+            [
+                'name' => 'CLAIRE LI',
+                'fb' => 'https://fb.com',
+                'twitter' => 'https://twitter.com',
+                'ig' => 'https://instagram.com',
+                'featured_image' => '/img/team-img-05.jpg'
+            ],
+            [
+                'name' => 'ELIZABETH SHU',
+                'fb' => 'https://fb.com',
+                'twitter' => 'https://twitter.com',
+                'ig' => 'https://instagram.com',
+                'featured_image' => '/img/team-img-06.jpg'
+            ],
+            [
+                'name' => 'DANIA LIU',
+                'fb' => 'https://fb.com',
+                'twitter' => 'https://twitter.com',
+                'ig' => 'https://instagram.com',
+                'featured_image' => '/img/team-img-07.jpg'
+            ],
+            [
+                'name' => 'MIRACLE YOUNG',
+                'fb' => 'https://fb.com',
+                'twitter' => 'https://twitter.com',
+                'ig' => 'https://instagram.com',
+                'featured_image' => '/img/team-img-08.jpg'
+            ],
+            [
+                'name' => 'CHARLIE PUTH',
+                'fb' => 'https://fb.com',
+                'twitter' => 'https://twitter.com',
+                'ig' => 'https://instagram.com',
+                'featured_image' => '/img/team-img-09.jpg'
+            ],
+            [
+                'name' => 'ASA BUTTERFIELD',
+                'fb' => 'https://fb.com',
+                'twitter' => 'https://twitter.com',
+                'ig' => 'https://instagram.com',
+                'featured_image' => '/img/team-img-10.jpg'
+            ]
+        ]);
     }
 }
