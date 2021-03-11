93fca7145a45d2a241e2b8fe931b61a0480649ac
Tita Wijayanti <wijayantitita094@gmail.com>
2021-03-11 11:43:50 +0700

    Meng-insert data pelanggan dalam seeder dan Memanggil pelangganSeeder dalam DatabaseSeeder.php

--- database/seeders/PelangganSeeder.php
+++ database/seeders/PelangganSeeder.php
@@ -13,6 +13,57 @@ class PelangganSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\Pelanggan::insert([
+            [
+                'title' => 'BEST',
+                'content' => 'HTML TEMPLATE',
+                'featured_image' => '/img/gallery-item-01.jpg'
+            ],
+            [
+                'title' => 'TOP',
+                'content' => 'CSS THEME',
+                'featured_image' => '/img/gallery-item-02.jpg'
+            ],
+            [
+                'title' => 'BEST',
+                'content' => 'HTML TEMPLATE',
+                'featured_image' => '/img/gallery-item-03.jpg'
+            ],
+            [
+                'title' => 'GREAT',
+                'content' => 'WEB THEME',
+                'featured_image' => '/img/gallery-item-04.jpg'
+            ],
+            [
+                'title' => 'BEST',
+                'content' => 'HTML COLLECTION',
+                'featured_image' => '/img/gallery-item-05.jpg'
+            ],
+            [
+                'title' => 'MAX',
+                'content' => 'CSS LAYOUT',
+                'featured_image' => '/img/gallery-item-06.jpg'
+            ],
+            [
+                'title' => 'BEST',
+                'content' => 'HTML LAYOUT',
+                'featured_image' => '/img/gallery-item-07.jpg'
+            ],
+            [
+                'title' => 'BEST',
+                'content' => 'CSS TEMPLATE',
+                'featured_image' => '/img/gallery-item-08.jpg'
+            ],
+            [
+                'title' => 'GREAT',
+                'content' => 'HTML DESIGN',
+                'featured_image' => '/img/gallery-item-09.jpg'
+            ],
+            [
+                'title' => 'TOP',
+                'content' => 'CSS COLLECTION',
+                'featured_image' => '/img/gallery-item-10.jpg'
+            ]
+        ]);
     }
 }
