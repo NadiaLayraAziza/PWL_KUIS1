ec7dde70a51718904f2c685ee6449b2e532fd174
Tita Wijayanti <wijayantitita094@gmail.com>
2021-03-12 16:11:36 +0700

    Membuat view halaman pelanggan

---
+++ resources/views/pelanggan.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('content')
+    <div class="bg-light text-white" id="family">
+            <br><br>
+            <span><h1 align="center">Tabel Pelanggan</h1></span>
+            <br><br>
+            <table class="table table-bordered table-striped table-hover text-center">
+                <tr>
+                    <th>No.</th><th>Id Pelanggan</th><th>Nama</th><th>Alamat</th><th>Nomor HP</th>
+                </tr>
+                @foreach ($pelanggan as $item)
+                    <tr>
+                        <td>{{$item->id}}</td><td><img src="{{$item->id_pelanggan}}"></td><td>{{$item->nama}}</td><td>{{$item->alamat}}</td><td>{{$item->nomor_hp}}</td>
+                    </tr>
+                @endforeach
+            </table>
+            <div class="row">
+                <div class="col-12">
+                    {{$pelanggan->links('paginate')}}
+                </div>
+            </div>
+    </div>
+@endsection
